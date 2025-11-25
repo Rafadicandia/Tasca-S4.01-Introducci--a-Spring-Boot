de44cbe638f3fb50763defd68a2e5770edd5e953
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-25 13:34:16 +0100

    feat: add PostMaping method for handling http request from user

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,7 +1,10 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.models.User;
+import cat.itacademy.s04.t01.userapi.models.UserRequestForNewUser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,6 +18,13 @@ public class UserController {
     public ArrayList<User> getUsers() {
 
         return users;
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody UserRequestForNewUser user){
+
+        User newUser = new User();
+        return newUser;
     }
 
 
