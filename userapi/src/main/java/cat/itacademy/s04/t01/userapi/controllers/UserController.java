e461bfebf40b62871670bd7def8a4dbe26333af5
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-25 13:06:38 +0100

    feat: add userController class

---
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -0,0 +1,11 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import java.util.ArrayList;
+
+public class UserController {
+    private ArrayList<String> users;
+
+    public UserController() {
+        this.users = new ArrayList<>();
+    }
+}
