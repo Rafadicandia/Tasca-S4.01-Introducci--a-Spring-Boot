049608a7abf28d96c33207385efc0d6f6eefc503
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-25 11:35:33 +0100

    build test for conection calss

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/UserApiApplication.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/UserApiApplication.java
@@ -2,7 +2,9 @@ package cat.itacademy.s04.t01.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"cat.itacademy.s04.t01.userapi"})
 @SpringBootApplication
 public class UserApiApplication {
 
