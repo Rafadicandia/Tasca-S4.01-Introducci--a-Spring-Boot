2e2cbcf8bd42cc8a05cf7a71029c6f2c1c1ba09d
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-25 13:29:05 +0100

    feat: add new class userRequestForNewUser for handling creation of new user parameters

---
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/models/UserRequestForNewUser.java
@@ -0,0 +1,27 @@
+package cat.itacademy.s04.t01.userapi.models;
+
+public class UserRequestForNewUser {
+    private String name;
+    private String email;
+
+    public UserRequestForNewUser(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
