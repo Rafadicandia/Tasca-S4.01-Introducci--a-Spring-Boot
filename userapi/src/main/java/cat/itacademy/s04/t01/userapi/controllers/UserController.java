ea3fe1cd45ec5dbe79d784adfe68b83be4bab94b
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-25 13:11:02 +0100

    feat: add method for RestController

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,11 +1,29 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.Status;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 
+@RestController
 public class UserController {
     private ArrayList<String> users;
 
     public UserController() {
         this.users = new ArrayList<>();
     }
+
+    @GetMapping("/users")
+    public ArrayList<String> getUsers() {
+
+        return users;
+    }
+
+
+
+
+
+
+
 }
