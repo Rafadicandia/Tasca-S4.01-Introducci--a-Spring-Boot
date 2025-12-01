7f1bf4d315f150e08ae49e73b5026fc82a678e62
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-12-01 10:49:17 +0100

    feat: add userRepository interface

--- Nivell-3/userapi/src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
+++ Nivell-3/userapi/src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
@@ -2,13 +2,16 @@ package cat.itacademy.s04.t01.userapi.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest; // ✅ USA ESTA RUTA
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(HealthControllerTest.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class HealthControllerTest {
 
 
--- Nivell-3/userapi/src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
+++ Nivell-3/userapi/src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -17,7 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
     private User testUser;
