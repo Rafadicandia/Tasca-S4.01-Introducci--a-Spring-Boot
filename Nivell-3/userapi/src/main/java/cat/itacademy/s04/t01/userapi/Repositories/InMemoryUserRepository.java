cd00d294c1a67e9d04fb03769b9ff516d9e6d8a1
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-12-03 10:14:07 +0100

    feature: add method User save

--- Nivell-3/userapi/src/main/java/cat/itacademy/s04/t01/userapi/Repositories/InMemoryUserRepository.java
+++ Nivell-3/userapi/src/main/java/cat/itacademy/s04/t01/userapi/Repositories/InMemoryUserRepository.java
@@ -4,17 +4,27 @@ import cat.itacademy.s04.t01.userapi.Interfaces.UserRepository;
 import cat.itacademy.s04.t01.userapi.models.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
+    private HashMap<UUID, User> userHashMap = new HashMap<>();
 
 
     @Override
     public User save(User user) {
-        return null;
+        if (user.getId() == null) {
+            UUID newId = UUID.randomUUID();
+            User newUser = new User(newId, user.getName(), user.getEmail());
+            userHashMap.put(newId, newUser);
+            return newUser;
+        } else  {
+            userHashMap.replace(user.getId(), user);
+            return user;
+        }
     }
 
     @Override
