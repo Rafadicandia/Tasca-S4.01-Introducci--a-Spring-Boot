a0ceac25d60e7075e3e587158a988e44cb38356a
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-12-29 20:35:41 +0100

    feat: add UserServiceImpl for implementing operations in repository base as service

---
+++ Nivell-3/userapi/src/main/java/cat/itacademy/s04/t01/userapi/services/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package cat.itacademy.s04.t01.userapi.services;
+
+import cat.itacademy.s04.t01.userapi.Interfaces.UserRepository;
+import cat.itacademy.s04.t01.userapi.Interfaces.UserService;
+import cat.itacademy.s04.t01.userapi.exceptions.EmailAlreadyExistsException;
+import cat.itacademy.s04.t01.userapi.models.User;
+import cat.itacademy.s04.t01.userapi.models.UserRequestForNewUser;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User save(UserRequestForNewUser userRequest) {
+        if (userRepository.existsByEmail(userRequest.getEmail())) {
+            throw new EmailAlreadyExistsException("Email already exist");
+        }
+        UUID uuid = randomUUID();
+        User newUser = new User(uuid, userRequest.getName(), userRequest.getEmail());
+        return userRepository.save(newUser);
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> searchByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
+}
