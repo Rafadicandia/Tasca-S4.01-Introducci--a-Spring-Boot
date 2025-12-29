24550f3467fce8c45d6f673aaffb65b5d78b116d
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-12-29 20:32:54 +0100

    feat: add EmailAlreadyExistsException

---
+++ Nivell-3/userapi/src/main/java/cat/itacademy/s04/t01/userapi/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package cat.itacademy.s04.t01.userapi.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
