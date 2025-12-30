9a1756ee99ea7fdacd40428cfe4348a7ce61e55e
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-12-30 16:02:23 +0100

    test: add @AutoConfigureMockMvc

--- Nivell-3/userapi/src/test/java/cat/itacademy/s04/t01/userapi/UserApiApplicationTests.java
+++ Nivell-3/userapi/src/test/java/cat/itacademy/s04/t01/userapi/UserApiApplicationTests.java
@@ -1,9 +1,11 @@
 package cat.itacademy.s04.t01.userapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UserApiApplicationTests {
 
 	@Test
