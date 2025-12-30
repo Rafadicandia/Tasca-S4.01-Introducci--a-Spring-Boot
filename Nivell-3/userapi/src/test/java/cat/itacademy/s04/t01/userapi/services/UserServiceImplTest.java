b64ce5543d8c4ef5a4d533474170f3df75df61ba
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-12-30 16:01:49 +0100

    test: add createUser_shouldSaveNewUserWhenEmailIsNotRegistered()

--- Nivell-3/userapi/src/test/java/cat/itacademy/s04/t01/userapi/services/UserServiceImplTest.java
+++ Nivell-3/userapi/src/test/java/cat/itacademy/s04/t01/userapi/services/UserServiceImplTest.java
@@ -1,6 +1,8 @@
 package cat.itacademy.s04.t01.userapi.services;
 
 import cat.itacademy.s04.t01.userapi.Interfaces.UserRepository;
+import cat.itacademy.s04.t01.userapi.exceptions.EmailAlreadyExistsException;
+import cat.itacademy.s04.t01.userapi.models.User;
 import cat.itacademy.s04.t01.userapi.models.UserRequestForNewUser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -25,9 +27,35 @@ class UserServiceImplTest {
     @DisplayName("Should throw exception when Email is already exists")
     void createUser_shouldThrowExceptionWhenEmailAlreadyExists() {
         UserRequestForNewUser userRequest = new UserRequestForNewUser("Pepito", "pepito@gmail.com");
+
         when(userRepository.existsByEmail(userRequest.getEmail())).thenReturn(true);
 
-        
+        EmailAlreadyExistsException emailAlreadyExistsException = assertThrowsExactly(
+                EmailAlreadyExistsException.class, () -> userService.save(userRequest));
+
+        assertEquals("Email already exist", emailAlreadyExistsException.getMessage());
+
+        verify(userRepository, never()).save(any(User.class));
+
+    }
+
+    @Test
+    @DisplayName("Should save new user when Email is not registered")
+    void createUser_shouldSaveNewUserWhenEmailIsNotRegistered() {
+        UserRequestForNewUser userRequest = new UserRequestForNewUser("Pepito", "pepito@gmail.com");
+
+        when(userRepository.existsByEmail(userRequest.getEmail())).thenReturn(false);
+
+        when(userRepository.save(any(User.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        User registeredUser = userService.save(userRequest);
+
+        assertNotNull(registeredUser.getId());
+        assertEquals("Pepito", registeredUser.getName());
+        assertEquals("pepito@gmail.com", registeredUser.getEmail());
+
+        verify(userRepository, times(1)).save(any(User.class));
+
 
     }
 
